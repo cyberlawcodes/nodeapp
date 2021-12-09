74a082352ab23f4d0176d3f051566319822f7aca
Emma Enriquez <cyberlawcodes@gmail.com>
2021-12-09 11:08:53 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-/*
 const rect = {
     perimeter: (x, y) => 2 * (x + y),
     area: (x, y) => x * y
@@ -6,35 +5,13 @@ const rect = {
 
 function solveRect(l, w) {
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
-    if (l <=0 || w <= 0) {
-        console.log(`Rectangle dimensions must be greater than zero.  Received ${l}, ${w}`);
+    if (l <= 0 || w <= 0) {
+        console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`);
     } else {
         console.log(`Area of rectangle: ${rect.area(l, w)}`);
         console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
     }
-}
-
-solveRect(2, 4);
-solveRect(3, 5);
-solveRect(0, 5);
-solveRect(5, -3);
-*/
-
-const rect = require('./rectangle');
-
-function solveRect(l, w) {
-    console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
-    
-    rect(l, w, (err, rectangle) => {
-        if (err) {
-            console.log('ERROR:', err.message);
-        } else {
-            console.log(`Area of rectangle with dimensions: ${l}, ${w}: ${rectangle.area()}`);
-            console.log(`Permimeter of rectangular with dimensions: ${l}, ${w}: ${rectangle.perimeter()}`);
-        }
-    });
-    console.log(`This statement is logged after the call to rect()`);
-}
+};
 
 solveRect(2, 4);
 solveRect(3, 5);
