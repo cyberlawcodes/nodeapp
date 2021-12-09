28f7a83a2e46e7948280b43e88a93f80c3be0c66
Emma Enriquez <cyberlawcodes@gmail.com>
2021-12-09 11:23:16 -0500

    changed app from to modular

--- rectangle.js
+++ rectangle.js
@@ -1,18 +1,6 @@
-module.exports = (x, y, callback) => {
-    if (x <= 0 || y <= 0) {
-        callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`));
-    } else {
-        setTimeout(() =>
-            callback(null, {
-                perimeter: () => 2 * (x + y),
-                area: () => x * y
-            }),
-            2000
-        );
-    }
-}
+module.exports.perimeter = (x, y) => 2 * (x + y);
+module.exports.area = (x, y) => x * y;
 
-perimeter = (x, y) => 2 * (x + y);
-area = (x, y) => x * y;
+
 
 
